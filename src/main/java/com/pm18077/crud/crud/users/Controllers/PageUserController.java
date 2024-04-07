7d6f3ca75852ee53678d8cd6e7b2e7dc7e7e29fc
Javier Peraza <contacto.peraza@gmail.com>
2024-04-07 00:15:31 -0600

    Vista de tabla creada, conexión de la vista a la BD exitosa, controladores de la vista funcionando

--- src/main/java/com/pm18077/crud/crud/users/Controllers/PageUserController.java
+++ src/main/java/com/pm18077/crud/crud/users/Controllers/PageUserController.java
@@ -1,17 +1,23 @@
 package com.pm18077.crud.crud.users.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.pm18077.crud.crud.users.repository.UserRepository;
+
 @Controller
 @RequestMapping("/")
 public class PageUserController {
     
+    @Autowired
+    private UserRepository userRepository;
     @GetMapping("/users")
 
     public String listUsers(Model model){
+        model.addAttribute("users", userRepository.findAll());
         return "list-users";
     }
     
