daeaa40e90dcb3fd091727559b5d3c64f7a06b32
Javier Peraza <contacto.peraza@gmail.com>
2024-04-07 21:21:20 -0600

    Formulario de ingreso de usuario listo, CR del crud están listos, falta UD

--- src/main/java/com/pm18077/crud/crud/users/Controllers/PageUserController.java
+++ src/main/java/com/pm18077/crud/crud/users/Controllers/PageUserController.java
@@ -4,9 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.pm18077.crud.crud.users.entities.User;
 import com.pm18077.crud.crud.users.repository.UserRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Controller
 @RequestMapping("/")
@@ -14,11 +19,23 @@ public class PageUserController {
     
     @Autowired
     private UserRepository userRepository;
-    @GetMapping("/users")
 
+    @GetMapping("/users")
     public String listUsers(Model model){
         model.addAttribute("users", userRepository.findAll());
         return "list-users";
+    }
+
+    @GetMapping("/nuevo")
+    public String formularioNuevoUsuario(Model model){
+        model.addAttribute("usuario", new User());
+        return "form-user";
+    }
+
+    @PostMapping("/nuevo")
+    public String guardarUser(@ModelAttribute User user) {
+        userRepository.save(user);
+        return "redirect:/users";
     }
     
 }
